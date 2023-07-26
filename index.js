4f900925477ae4a797de2d3e9f308176b66b80eb
karthik-mohan23 <karthikmohan2310@gmail.com>
2023-07-26 21:16:44 +0530

    completed

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ const express = require("express");
 
 const app = express();
 
+const calculateSum = (num) => {
+  let sum = 0;
+  for (i = 0; i <= num; i++) {
+    sum += i;
+  }
+  return sum;
+};
+
 app.get("/", (req, res) => {
-  res.send("<h1>Hello World</h1>");
+  const counter = req.query.counter;
+  const answer = calculateSum(counter);
+  res.send(`The sum of first  ${counter} numbers is ${answer}`);
 });
 
 app.listen(3000, () => console.log("Started server"));
